8ff6dde4184f7fdbdc0566ac49db65b20a34830d
Paul Dudnic <paul.dudnic@grenoble-inp.org>
2021-02-08 19:55:42 +0100

    Layer Structure Basic

--- NeuralNetworks/NeuralNetwork/Layers/WeightDecayLayer.cs
+++ NeuralNetworks/NeuralNetwork/Layers/WeightDecayLayer.cs
@@ -1,57 +1,41 @@
 ﻿using MathNet.Numerics.LinearAlgebra;
 using NeuralNetwork.Common.Layers;
 using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace NeuralNetwork.Layers
 {
-    internal class WeightDecayLayer : ILayer, IEquatable<WeightDecayLayer>
+    class WeightDecayLayer : ILayer
     {
-        public WeightDecayLayer(ILayerWithStorage underlyingLayer, double decayRate)
+        public int LayerSize { get; }
+
+        public int InputSize { get; }
+
+        public int BatchSize { get; set; }
+
+        public Matrix<double> Activation { get; }
+
+        public Matrix<double> WeightedError { get; }
+
+        public WeightDecayLayer()
         {
-            UnderlyingLayer = underlyingLayer ?? throw new ArgumentNullException(nameof(underlyingLayer));
-            DecayRate = decayRate;
-            DecayedWeights = Matrix<double>.Build.Dense(UnderlyingLayer.MatrixStorage.Weights.RowCount, UnderlyingLayer.MatrixStorage.Weights.ColumnCount);
+
         }
-
-        public Matrix<double> DecayedWeights { get; }
-        public ILayerWithStorage UnderlyingLayer { get; }
-        public double DecayRate { get; }
-
-        public int LayerSize => UnderlyingLayer.LayerSize;
-
-        public int InputSize => UnderlyingLayer.InputSize;
-
-        public int BatchSize { get => UnderlyingLayer.BatchSize; set => UnderlyingLayer.BatchSize = value; }
-
-        public Matrix<double> Activation => UnderlyingLayer.Activation;
-
-        public Matrix<double> WeightedError => UnderlyingLayer.WeightedError;
 
         public void BackPropagate(Matrix<double> upstreamWeightedErrors)
         {
-            UnderlyingLayer.BackPropagate(upstreamWeightedErrors);
-        }
-
-        public bool Equals(ILayer other)
-        {
-            return other is WeightDecayLayer weight && Equals(weight);
-        }
-
-        public bool Equals(WeightDecayLayer other)
-        {
-            return UnderlyingLayer.Equals(other.UnderlyingLayer) && DecayRate == other.DecayRate;
+            throw new NotImplementedException();
         }
 
         public void Propagate(Matrix<double> input)
         {
-            UnderlyingLayer.Propagate(input);
+            throw new NotImplementedException();
         }
 
         public void UpdateParameters()
         {
-            UnderlyingLayer.MatrixStorage.Weights.Multiply(DecayRate, DecayedWeights);
-            UnderlyingLayer.UpdateParameters();
-            UnderlyingLayer.MatrixStorage.Weights.Subtract(DecayedWeights, UnderlyingLayer.MatrixStorage.Weights);
+            throw new NotImplementedException();
         }
     }
 }
